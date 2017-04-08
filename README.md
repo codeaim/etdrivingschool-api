dedfe93f43f9606aab3482feac6d0d6cd27ae908
gdownes <gdownes@gmail.com>
2017-04-08 17:52:04 +0100

    Adding valid api template.yml and swagger.yml

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Introduction
-**etdrivingschool-api** is used to provision AWS Web API Gateway and related AWS Lambda functions to support the etdrivingschool-web project.
+**etdrivingschool-api** is used to provision AWS API Gateway and related AWS Lambda functions to support the etdrivingschool-web project.
 
 # Installation
 Clone the repository
@@ -24,20 +24,28 @@ zip -r deploy.zip index.js node_modules
 
 Create AWS S3 bucket
 ```bash
-aws s3api create-bucket --bucket etdrivingschool-api --region eu-west-1 --create-bucket-configuration LocationConstraint=eu-west-1
+aws s3api create-bucket --bucket etdrivingschool-api
 ```
 
 Upload deployment package to AWS S3 bucket
 ```bash
 aws s3 cp deploy.zip s3://etdrivingschool-api/deploy.zip
 ```
+Upload AWS API Gateway OpenAPI specification to AWS S3 bucket
+```bash
+aws s3 cp swagger.yml s3://etdrivingschool-api/swagger.yml
 
 Upload AWS CloudFormation template to AWS S3 bucket
 ```bash
 aws s3 cp template.yml s3://etdrivingschool-api/template.yml
 ```
 
-Create etdrivingschool-api stack using AWS CloudFormation template
+Create etdrivingschool-api stack using AWS CloudFormation template. Replace parameters below with valid values.
 ```bash
-aws cloudformation create-stack --stack-name etdrivingschool-api --template-url https://s3.amazonaws.com/etdrivingschool-api/template.yml --capabilities CAPABILITY_IAM
+aws cloudformation create-stack --stack-name etdrivingschool-api --template-url https://s3.amazonaws.com/etdrivingschool-api/template.yml --capabilities CAPABILITY_IAM --parameters ParameterKey=MailgunApiKeyParameter,ParameterValue=MailgunApiKeyParameter ParameterKey=MailgunDomainParameter,ParameterValue=MailgunDomainParameter ParameterKey=TwitterConsumerKeyParameter,ParameterValue=TwitterConsumerKeyParameter ParameterKey=TwitterConsumerSecretParameter,ParameterValue=TwitterConsumerSecretParameter ParameterKey=TwitterAccessTokenKeyParameter,ParameterValue=TwitterAccessTokenKeyParameter ParameterKey=TwitterAccessTokenSecretParameter,ParameterValue=TwitterAccessTokenSecretParameter
+```
+
+View details of provisioned AWS API Gateway
+```bash
+aws apigateway get-rest-apis
 ```
--- index.js
+++ index.js
@@ -1,30 +1,59 @@
 'use strict';
 
-const twitter = require('twitter');
-
 module.exports.tweet = (event, context, callback) => {
-    const client = new Twitter({
-        consumer_key: process.env.CONSUMER_KEY,
-        consumer_secret: process.env.CONSUMER_SECRET,
-        access_token_key: process.env.ACCESS_TOKEN_KEY,
-        access_token_secret: process.env.ACCESS_TOKEN_SECRET
-    });
+	const Twitter = require('twitter');
 
-    const params = {
-        screen_name: 'etdrivingschool'
-    }
-    
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
-        if (error) {
-            console.log(error);
-            callback(error);
-        }
+	const client = new Twitter({
+		consumer_key: process.env.TWITTER_CONSUMER_KEY,
+		consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+		access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+		access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+	});
 
-        const result = {
-            "statusCode": 200,
-            "headers": {},
-            "body": JSON.stringify(tweets)
-        };
-        callback(null, result);
-    });
+	const params = {
+		screen_name: 'etdrivingschool'
+	}
+
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (error) {
+			console.log(error);
+			callback(error);
+		}
+
+		const result = {
+			"statusCode": 200,
+			"headers": {},
+			"body": JSON.stringify(tweets)
+		};
+		callback(null, result);
+	});
+};
+
+module.exports.contact = (event, context, callback) => {
+	const requestBody = JSON.parse(event.body);
+	const mailgun = require('mailgun-js')({
+		apiKey: process.env.MAILGUN_API_KEY,
+		domain: process.env.MAILGUN_DOMAIN
+	});
+
+	const data = {
+		from: 'ET Driving School Contact Form <admin@codeaim.com>',
+		to: 'gdownes@gmail.com',
+		subject: `${requestBody.title} (${requestBody.email})`,
+		text: requestBody.message
+	};
+
+	mailgun.messages().send(data, function(error, body) {
+		if (error) {
+			console.log(error);
+			callback(error);
+		}
+
+		const result = {
+			"statusCode": 201,
+			"headers": {},
+			"body": ''
+		}
+		callback(null, result);
+	});
 };
